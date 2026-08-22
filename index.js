0f44b3f272981f9f1d9e7cb9c6c20289f5042ba8
azyrov <for28@yahoo.com>
2026-08-22 13:31:50 +0000

    Add all boxes

---
+++ index.js
@@ -0,0 +1,65 @@
+let div = document.querySelector("#container");
+
+let boxOne = document.createElement("div");
+boxOne.classList.add("box");
+div.append(boxOne);
+
+let boxTwo = document.createElement("div");
+boxTwo.classList.add("box");
+div.append(boxTwo);
+
+let boxThree = document.createElement("div");
+boxThree.classList.add("box");
+div.append(boxThree);
+
+let boxFour = document.createElement("div");
+boxFour.classList.add("box");
+div.append(boxFour);
+
+let boxFive = document.createElement("div");
+boxFive.classList.add("box");
+div.append(boxFive);
+
+let boxSix = document.createElement("div");
+boxSix.classList.add("box");
+div.append(boxSix);
+
+let boxSeven = document.createElement("div");
+boxSeven.classList.add("box");
+div.append(boxSeven);
+
+let boxEight = document.createElement("div");
+boxEight.classList.add("box");
+div.append(boxEight);
+
+let boxNine = document.createElement("div");
+boxNine.classList.add("box");
+div.append(boxNine);
+
+let boxTen = document.createElement("div");
+boxTen.classList.add("box");
+div.append(boxTen);
+
+let boxEleven = document.createElement("div");
+boxEleven.classList.add("box");
+div.append(boxEleven);
+
+let boxTwelve = document.createElement("div");
+boxTwelve.classList.add("box");
+div.append(boxTwelve);
+
+let boxThirteen = document.createElement("div");
+boxThirteen.classList.add("box");
+div.append(boxThirteen);
+
+let boxFourten = document.createElement("div");
+boxFourten.classList.add("box");
+div.append(boxFourten);
+
+let boxFifteen = document.createElement("div");
+boxFifteen.classList.add("box");
+div.append(boxFifteen);
+
+let boxSixteen = document.createElement("div");
+boxSixteen.classList.add("box");
+div.append(boxSixteen);
