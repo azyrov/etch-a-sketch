84fdb1a50f950e748471e136a1e78c1a33d96811
azyrov <for28@yahoo.com>
2026-08-22 16:31:28 +0000

    Generate boxes
    Add basic styling for boxes

--- index.js
+++ index.js
@@ -1,77 +1,48 @@
 let div = document.querySelector("#container");
 
-let boxOne = document.createElement("div");
-boxOne.classList.add("box");
-div.append(boxOne);
 
-let boxTwo = document.createElement("div");
-boxTwo.classList.add("box");
-div.append(boxTwo);
+// store value used for both width and height
+// square that value to get the total number of boxes 
+// use document fragment to create a number of boxesbased on the squared num
+// append created divs to fragment
+// append document fragment to live dom
 
-let boxThree = document.createElement("div");
-boxThree.classList.add("box");
-div.append(boxThree);
+const fragment = document.createDocumentFragment();
 
-let boxFour = document.createElement("div");
-boxFour.classList.add("box");
-div.append(boxFour);
+const num = 16;
+const numSquared = num ** 2;
 
-let boxFive = document.createElement("div");
-boxFive.classList.add("box");
-div.append(boxFive);
+for (let i = 0; i < numSquared; ++i) {
+    const newDiv = document.createElement("div");
 
-let boxSix = document.createElement("div");
-boxSix.classList.add("box");
-div.append(boxSix);
+    fragment.append(newDiv);
+}
 
-let boxSeven = document.createElement("div");
-boxSeven.classList.add("box");
-div.append(boxSeven);
+div.append(fragment);
 
-let boxEight = document.createElement("div");
-boxEight.classList.add("box");
-div.append(boxEight);
+const newDiv = div.childNodes; 
 
-let boxNine = document.createElement("div");
-boxNine.classList.add("box");
-div.append(boxNine);
-
-let boxTen = document.createElement("div");
-boxTen.classList.add("box");
-div.append(boxTen);
-
-let boxEleven = document.createElement("div");
-boxEleven.classList.add("box");
-div.append(boxEleven);
-
-let boxTwelve = document.createElement("div");
-boxTwelve.classList.add("box");
-div.append(boxTwelve);
-
-let boxThirteen = document.createElement("div");
-boxThirteen.classList.add("box");
-div.append(boxThirteen);
-
-let boxFourten = document.createElement("div");
-boxFourten.classList.add("box");
-div.append(boxFourten);
-
-let boxFifteen = document.createElement("div");
-boxFifteen.classList.add("box");
-div.append(boxFifteen);
-
-let boxSixteen = document.createElement("div");
-boxSixteen.classList.add("box");
-div.append(boxSixteen);
-
-let buttons = div.childNodes;
-
-buttons.forEach((btn) => {
-    btn.addEventListener("mouseenter", function (e) {
-        this.classList.add("hover");
-    })
-
-    btn.addEventListener("mouseleave", function (e) {
-        this.classList.remove("hover");
-    })
+newDiv.forEach((div) => {
+    div.classList.add("box")
 })
+
+function setFlexItemsPerLine(itemsPerLine){
+    const percentage = (100 / itemsPerLine).toFixed(4);
+    console.log(percentage)
+
+    newDiv.forEach((div) => {
+        div.style.flexBasis = `${percentage}%`;
+        div.style.flexGrow = "0";
+        div.style.flexShrink = "0"
+    })
+}
+
+setFlexItemsPerLine(5)
+
+
+
+// add btn
+// add prompt to colelct number
+/// the number determines both eidth and height
+// use if to add conditions for max value to prompt
+// store
